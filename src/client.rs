802e8bfc3f8a18b2aabab25eb6f9d476e4d23857
Adam Avramov <adam.b.avramov@gmail.com>
2019-03-10 23:38:14 +0200

    update to reqwest 0.9

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
 use errors::*;
 use reqwest;
 use reqwest::{StatusCode, Response};
-use reqwest::header::{Headers, UserAgent, ContentType};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, USER_AGENT, CONTENT_TYPE};
 use std::io::Read;
 use ring::{digest, hmac};
 use hex::encode as hex_encode;
@@ -12,7 +12,7 @@ static API_SIGNATURE_PATH : &'static str = "/api/v2/auth/r/";
 
 #[derive(Clone)]
 pub struct Client {
-    api_key: String, 
+    api_key: String,
     secret_key: String
 }
 
@@ -29,7 +29,7 @@ impl Client {
         if !request.is_empty() {
             url.push_str(format!("?{}", request).as_str());
         }
- 
+
         let response = reqwest::get(url.as_str())?;
 
         self.handler(response)
@@ -44,54 +44,54 @@ impl Client {
             .body(payload)
             .send()?;
 
-        self.handler(response)            
-    } 
+        self.handler(response)
+    }
 
-    fn build_headers(&self, request: String, payload: String) -> Result<Headers> {
+    fn build_headers(&self, request: String, payload: String) -> Result<HeaderMap> {
         let nonce: String = self.generate_nonce()?;
         let signature_path: String = format!("{}{}{}{}", API_SIGNATURE_PATH, request, nonce, payload);
 
         let signed_key = hmac::SigningKey::new(&digest::SHA384, self.secret_key.as_bytes());
         let signature = hex_encode(hmac::sign(&signed_key, signature_path.as_bytes()).as_ref());
 
-        let mut custon_headers = Headers::new();  
-        custon_headers.set(UserAgent::new("bitfinex-rs"));
-        custon_headers.set_raw("bfx-nonce", nonce.as_str());
-        custon_headers.set_raw("bfx-apikey", self.api_key.as_str());
-        custon_headers.set_raw("bfx-signature", signature.as_str());
-        custon_headers.set(ContentType::json());
+        let mut headers = HeaderMap::new();
+        headers.insert(USER_AGENT, HeaderValue::from_static("bitfinex-rs"));
+        headers.insert(HeaderName::from_static("bfx-nonce"), HeaderValue::from_str(nonce.as_str())?);
+        headers.insert(HeaderName::from_static("bfx-apikey"), HeaderValue::from_str(self.api_key.as_str())?);
+        headers.insert(HeaderName::from_static("bfx-signature"), HeaderValue::from_str(signature.as_str())?);
+        headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
-        Ok(custon_headers)
-    } 
+        Ok(headers)
+    }
 
     fn generate_nonce(&self) -> Result<String> {
         let start = SystemTime::now();
         let since_epoch = start.duration_since(UNIX_EPOCH)?;
-    
+
         let timestamp = since_epoch.as_secs() * 1000 + since_epoch.subsec_nanos() as u64 / 1_000_000;
 
-        Ok((timestamp + 1).to_string())      
+        Ok((timestamp + 1).to_string())
     }
 
     fn handler(&self, mut response: Response) -> Result<(String)> {
         match response.status() {
-            StatusCode::Ok => {
+            StatusCode::OK => {
                 let mut body = String::new();
                 response.read_to_string(&mut body)?;
                 return Ok(body);
             },
-            StatusCode::InternalServerError => {
+            StatusCode::INTERNAL_SERVER_ERROR => {
                 bail!("Internal Server Error");
             }
-            StatusCode::ServiceUnavailable => {
+            StatusCode::SERVICE_UNAVAILABLE => {
                 bail!("Service Unavailable");
             }
-            StatusCode::Unauthorized => {
+            StatusCode::UNAUTHORIZED => {
                 bail!("Unauthorized");
-            }            
-            StatusCode::BadRequest => {
+            }
+            StatusCode::BAD_REQUEST => {
                 bail!(format!("Bad Request: {:?}", response));
-            }                        
+            }
             s => {
                 bail!(format!("Received response: {:?}", s));
             }
