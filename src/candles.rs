f7f145a302bfa778e32dd4d23264f8be426f0551
Afonso Bordado <afonsobordado@az8.co>
2020-08-05 10:25:23 +0100

    Add Candle history request params

--- src/candles.rs
+++ src/candles.rs
@@ -2,6 +2,49 @@ use client::*;
 use errors::*;
 use serde_json::from_str;
 
+#[derive(Debug, Clone, Default)]
+pub struct CandleHistoryParams {
+    /// Number of candles requested (Max: 10000)
+    pub limit: Option<i32>,
+
+    /// Filter start (ms)
+    pub start: Option<i64>,
+
+    /// Filter end (ms)
+    pub end: Option<i64>,
+
+    /// Sorts the results from old > new
+    pub sort: Option<bool>,
+}
+
+impl CandleHistoryParams {
+    pub fn new() -> Self {
+        Self {
+            limit: Some(120),
+            sort: Some(false),
+            start: None,
+            end: None,
+        }
+    }
+
+    pub fn to_query(&self) -> String {
+        format!("{}={}&{}={}&{}={}&{}={}",
+            "limit", self.limit
+                         .map(|a| a.to_string())
+                         .unwrap_or("".into()),
+            "start", self.start
+                         .map(|a| a.to_string())
+                         .unwrap_or("".into()),
+            "end", self.end
+                       .map(|a| a.to_string())
+                       .unwrap_or("".into()),
+            "sort", self.sort
+                        .map(|a| if a { "1" } else { "0" })
+                        .unwrap_or("".into()),
+        )
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Candle { 
     pub timestamp: i64,   
@@ -35,11 +78,16 @@ impl Candles {
         Ok(history)
     }    
 
-    pub fn history<S>(&self, symbol: S, timeframe: S) -> Result<Vec<Candle>>
+    pub fn history<S>(
+        &self,
+        symbol: S,
+        timeframe: S,
+        params: &CandleHistoryParams,
+    ) -> Result<Vec<Candle>>
         where S: Into<String>
     {    
         let endpoint: String = format!("candles/trade:{}:t{}/hist", timeframe.into(), symbol.into());
-        let data = self.client.get(endpoint, String::new())?;
+        let data = self.client.get(endpoint, params.to_query())?;
 
         let history: Vec<Candle> = from_str(data.as_str())?;
 
